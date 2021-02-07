fed920d6e7e075f79d973725ad18cd9c180326f6
supremeidev <joey.stoetzel@live.de>
2021-02-07 15:14:09 +0100

    SignIn Page

---
+++ src/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021.
+ * Joey Stoetzel, github.com/Joeyrstz
+ * This project is part of my portfolio and was developed for educational and presentation purposes.
+ * The use of the project for internal, external or profit-oriented purposes is prohibited without my consent.
+ */
+
